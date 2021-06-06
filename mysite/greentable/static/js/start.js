a7becb96d285f99d63070101973e952708ee9a6a
or.llon <dhflxhdxhd@gmail.com>
2021-06-06 19:52:59 +0900

    index.html & start.html 수정

--- mysite/greentable/static/js/start.js
+++ mysite/greentable/static/js/start.js
@@ -1,78 +1,12 @@
-const main = document.querySelector("#main");
-const qna = document.querySelector("#qna");
-const result = document.querySelector("#result");
-const start_button = document.querySelector(".js-start");
-var qnaBox = document.querySelector(".js-qnaBox");
-const rexplain = document.querySelector(".rexplain");
-const endPoint = 4;
-const select = [];
-
-result.style.display = "none";
-
-function setResult(){
-
-}
-
-function calResult(){
-
-}
-
-
-function goResult(){
-    qna.style.display = "none";
-    result.style.display = "block";
-
-
-    setResult();
-}
-
-function addAnswer(answerText, qId, Id){
-    var answer = document.querySelector(".js-answerBox");
-    var answerBtn = document.createElement('button');
-
-    answerBtn.classList.add('answerList');
-    answer.appendChild(answerBtn);
-    answerBtn.innerHTML = answerText;
-
-    answerBtn.addEventListener("click",function(){
-        var children = document.querySelectorAll(".answerList");
-
-        select[qId] = Id;  // 결과 페이지 : 선택한 번호.
-        console.log(select);
-
-        for(let i=0; i<children.length; i++){
-            children[i].disabled = true;
-            children[i].style.display = "none" ;
-        }
-        goNext(++qId);
-    },false);
-}
-
-function goNext(qId) {
-
-    if(qId == endPoint ){
-        goResult();
-        return;
-    }
-
-    qnaBox.innerHTML = qnaList[qId].q;
-    for (let i in qnaList[qId].a) {
-        addAnswer(qnaList[qId].a[i].answer,qId, i);
-    }
-}
+const btn = document.querySelector(".js-btn");
 
 function begin() {
   console.log("start");
-    main.style.display = "none";
-    qna.style.display = "block";
-
-
-    let qId = 0;
-    goNext(qId);
+  location.href="form/"
 }
 
 function init() {
-    start_button.addEventListener("click", begin);
+    btn.addEventListener("click", begin);
 }
 
 init();
